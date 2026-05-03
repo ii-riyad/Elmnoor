bcc844ce536f705e4fc9ef312a08162444d50622
Assayyaad <hi@assayyaad.pro>
2026-05-04 07:55:42 +0800

    إصلاح علامات التنسيق في رؤوس الصفحات

--- js/fa-fallback.js
+++ js/fa-fallback.js
@@ -10,7 +10,6 @@
       const fa = /** @type {HTMLLinkElement} */ (document.createElement('link'))
       fa.rel = 'stylesheet'
       fa.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
-      fa.integrity = 'sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=='
       fa.crossOrigin = 'anonymous'
       fa.referrerPolicy = 'no-referrer'
       document.head.appendChild(fa)
