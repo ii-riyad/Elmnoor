f588dc54cdc60d301419ca48913973b9c0585f3c
Assayyaad <hi@assayyaad.pro>
2026-05-10 14:42:10 +0800

    إزالة متتبعات جوجل

--- js/init.js
+++ js/init.js
@@ -1,21 +1,6 @@
 ;(() => {
   'use strict'
 
-  const trackingId = 'G-4J70S0ZD47'
-
-  // @ts-expect-error
-  window.dataLayer = window.dataLayer || []
-
-  function gtag() {
-    // @ts-expect-error
-    window.dataLayer.push(arguments)
-  }
-
-  // @ts-expect-error
-  window.gtag = gtag
-  gtag('js', new Date())
-  gtag('config', trackingId)
-
   // @ts-expect-error
   document.querySelectorAll('link[rel="preload"][as="style"]').forEach(function (/** @type {HTMLLinkElement} */ link) {
     if (document.readyState === 'loading') {
